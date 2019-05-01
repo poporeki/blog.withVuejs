95c257cd61ea7e21d5a545298bcba03fa166be7a
poporeki <yansk@live.com>
2019-05-01 15:18:28 +0800

    '190501'

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express'),
   app = express();
 const history = require('connect-history-api-fallback');
 
-const PORT=9001;
-app.use(history());
+const PORT = 9001;
+app.use(history({
+  history: './dist/index.html'
+}));
 app.use(express.static("./dist"));
-app.get('./', (req, res) =>res.sendFile("./index.html"));
-app.listen(PORT, () =>console.log('success'));
+
+app.listen(PORT, () => console.log('success'));
